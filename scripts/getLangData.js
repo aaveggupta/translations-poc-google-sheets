d30a507585a17175f6f6ab09666e7acbc3e6af26
aaveggupta <aaveggupta.contact@gmail.com>
2023-02-14 14:03:27 +0530

    redundant code removed

--- scripts/getLangData.js
+++ scripts/getLangData.js
@@ -8,26 +8,7 @@ async function fetchLangData() {
       "https://script.google.com/macros/s/AKfycbxexVdUh9ZCO-TPf2w8e4OV6DwL7S4nLiol2D1A2tqBOkbyd4p6u2mGYScqisnzSEkt/exec"
     );
     const res = await response.json();
-
-    // Create a new file in the src folder
-    // const handle = await window.showSaveFilePicker({
-    //   suggestedName: "langData.json",
-    //   types: [
-    //     {
-    //       description: "JSON Files",
-    //       accept: { "application/json": [".json"] },
-    //     },
-    //   ],
-    //   excludeAcceptAllOption: true,
-    // });
-
-    // // Write the data to the file
-    // const writable = await handle.createWritable();
-    // await writable.write(JSON.stringify(data.data));
-    // await writable.close();
     fs.writeFileSync(fileName, JSON.stringify(res.data, null, "\t"), "utf8");
-
-    console.log(res.data);
   } catch (error) {
     console.error(error);
   }
